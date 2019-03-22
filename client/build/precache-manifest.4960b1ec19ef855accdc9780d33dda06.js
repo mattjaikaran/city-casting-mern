183b1b4abe4447e9bd23f0111f133a1c4eaaa6e9
Matthew Jaikaran <mattjaikaran@gmail.com>
2019-03-22 17:02:32 -0400

    df

---
+++ client/build/precache-manifest.4960b1ec19ef855accdc9780d33dda06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b5bce173c8e9e6768ac4",
+    "url": "/static/js/main.c93dc6d2.chunk.js"
+  },
+  {
+    "revision": "a88c54d9e56613c8b13f",
+    "url": "/static/js/2.b1f123e8.chunk.js"
+  },
+  {
+    "revision": "b5bce173c8e9e6768ac4",
+    "url": "/static/css/main.ac483ee3.chunk.css"
+  },
+  {
+    "revision": "a88c54d9e56613c8b13f",
+    "url": "/static/css/2.6fdfa086.chunk.css"
+  },
+  {
+    "revision": "ab0f442d828db2afbf987138066f24e0",
+    "url": "/index.html"
+  }
+];
